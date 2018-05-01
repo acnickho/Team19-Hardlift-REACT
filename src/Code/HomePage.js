86b8790c78afbc0183fbce6834bd1f90b74be875
beardedtomato <38308187+beardedtomato@users.noreply.github.com>
2018-05-01 18:43:48 +1000

    fiddled with fixing homepage issues

--- src/Code/HomePage.js
+++ src/Code/HomePage.js
@@ -5,21 +5,27 @@ const HomePage = () => (
 <div>
     <h1 className="h1">Hard Lift</h1>
 	<div className="aaa">
-	Slide show here? Photos of customers during program.
+		<p>Slide show here? Photos of customers during program.</p>
 	</div>
 
-	<div className="bbb">
-    Popular program + recommendation with reviews.
-	Photo as well?
+
+	<div className ="middlecontainer">
+		<div className="bbb">
+			<p>Popular program + recommendation with reviews.
+			Photo as well?</p>
+		</div>
+		
+		<div className="ccc">
+			<p>Newest program + recommendation + illustration</p>
+		</div>
 	</div>
 	
-	<div className="ccc">
-    Newest program + recommendation + illustration
+	<div className="center-div">
+		<div className="bb">
+			<p>Wanted to put this at the bot, but not working.</p>
+		</div>
 	</div>
 	
-	<div className="bb">
-    Wanted to put this at the bot, but not working.
-	</div>
 </div>
   
 )
