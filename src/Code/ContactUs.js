a917e299506c3a02a29895ae0b00e22f9b7e9760
beardedtomato <38308187+beardedtomato@users.noreply.github.com>
2018-05-01 18:46:00 +1000

    fixed visual issues

--- src/Code/ContactUs.js
+++ src/Code/ContactUs.js
@@ -6,31 +6,33 @@ import './contactus.css';
 const ContactUs = () => (
 <div>
 	<h1>Contact Us</h1>
-	<div className="one">
-		<div>
-		List_of_ contact number, opening hour,
-		email and stuff
-		</div>
-	</div>
-    <div className="two">	
-		This is a form
-		<div className="name">
-		Name/Email
-		</div>
-		
-		<div className="question">
-		Dropdown list questions
-		</div>		
-		
-		<div className="phone">
-		Phone number?
-		</div>
-
-		<div className="details">
-		Details_of_ questions?
-		</div>		
-	</div>
+	<div className="container">
 	
+		<div className="one">
+			<div>
+			List_of_ contact number, opening hour,
+			email and stuff
+			</div>
+		</div>
+		<div className="two">	
+			This is a form
+			<div className="name">
+			Name/Email
+			</div>
+			
+			<div className="question">
+			Dropdown list questions
+			</div>		
+			
+			<div className="phone">
+			Phone number?
+			</div>
+
+			<div className="details">
+			Details_of_ questions?
+			</div>		
+		</div>
+	</div>
 </div>
 )
 
