d2f315da34559cc4dc982481b7871f555bd1a811
dslatter <38239348+dslatter@users.noreply.github.com>
2018-05-03 10:42:27 +1000

    Contact Us Form
    Contact Us Form - Still need to add in client details and FAQ to page

--- src/Code/ContactUs.js
+++ src/Code/ContactUs.js
@@ -6,32 +6,40 @@ import './contactus.css';
 const ContactUs = () => (
 <div>
 	<h1>Contact Us</h1>
+
 	<div className="container">
-	
-		<div className="one">
-			<div>
-			List_of_ contact number, opening hour,
-			email and stuff
-			</div>
-		</div>
-		<div className="two">	
-			This is a form
-			<div className="name">
-			Name/Email
-			</div>
-			
-			<div className="question">
-			Dropdown list questions
-			</div>		
-			
-			<div className="phone">
-			Phone number?
+
+		<div className="two">
+			<h3>Contact Us</h3>
+
+			<div className="container">
+				<label for="fname">First Name</label>
+    		<input type="text" id="fname" name="firstname" placeholder="Your name..">
+				</input>
+
+			<label for="lname">Last Name</label>
+    	<input type="text" id="lname" name="lastname" placeholder="Your last name..">
+			</input>
+
+			<label for="email">Email</label>
+    		<input type="text" id="email" name="email" placeholder="Your Email Address..">
+				</input>
 			</div>
 
-			<div className="details">
-			Details_of_ questions?
-			</div>		
-		</div>
+						<label for="subject">Subject</label>
+			<select id="subject" name="subject">
+						<option value="generalenquiry">General Enquiry</option>
+						<option value="nutrition">Nutrition</option>
+						<option value="mediaenquiry">Media Enquiry</option>
+			</select>
+
+							<label for="message">Message</label>
+				<textarea rows="4" cols="50" placeholder="Write your Message Here...">
+				</textarea>
+
+			<input type="submit" value="Submit"></input>
+
+					</div>
 	</div>
 </div>
 )
