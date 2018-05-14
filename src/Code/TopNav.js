e9ae7cb8bd830397b35c0cd7cca3d41e86ec8a1d
beardedtomato <38308187+beardedtomato@users.noreply.github.com>
2018-05-14 15:50:31 +1000

    attempt to fix navbar

--- src/Code/TopNav.js
+++ src/Code/TopNav.js
@@ -8,26 +8,29 @@ import 'semantic-ui-css/semantic.min.css';
 // The Header creates links that can be used to navigate
 // between routes.
 const TopNav = () => (
-  <header classname = "topnav">
+		<header classname = "topnav">
+		<div className = "topnavcontainer">
 			<div classname="logo">
 				<Link to={"./HomePage"}>
 				<img src={logo} className="HL-logo" alt="logo" Link to={"./HomePage"} />
 				</Link>
 			</div>
 			<div className="topnav">
-				<table className="a"><tr>
+				<ul className="navlist">
 					
-					<td><Link to={"./HomePage"}><h4 >Home</h4></Link></td>
-					<td><Link to={"./AboutPage"}><h4>About</h4></Link></td>
-					<td><Link to={"./WorkoutPage"}><h4>Workouts</h4></Link></td>
-					<td><Link to={"./BlogPage"}><h4>Blog</h4></Link></td>
-					<td><Link to={"./ShopPage"}><h4>Shop</h4></Link></td>
-					<td><Link to={"./ContactUs"}><h4>Contact Us</h4></Link></td>
-					<td><Icon name="huge search"/></td>
-					<td><Icon name="huge shopping cart icon"/></td>
+					<li><Link to={"./HomePage"}><h4 >Home</h4></Link></li>
+					<li><Link to={"./AboutPage"}><h4>About</h4></Link></li>
+					<li><Link to={"./WorkoutPage"}><h4>Workouts</h4></Link></li>
+					<li><Link to={"./BlogPage"}><h4>Blog</h4></Link></li>
+					<li><Link to={"./ShopPage"}><h4>Shop</h4></Link></li>
+					<li><Link to={"./ContactUs"}><h4>Contact Us</h4></Link></li>
+					<li id="searchicon"><Icon name="huge search"/></li>
+					<li id="shoppingicon"><Icon name="huge shopping cart icon"/></li>
 					
-				</tr></table>
+				</ul>
 			</div>
+		</div>
+			
 		</header>
 )
 
